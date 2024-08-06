9da476929aabbba9c7bf2772563671784947e528
Ellie Racheeva <ellie.racheeva@gmail.com>
2024-08-06 07:16:36 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
-import About from "./pages/nailCovers";
+import nailCovers from "./pages/nailCovers";
 import './App.css'
 import Header from "./components/Header";
 
