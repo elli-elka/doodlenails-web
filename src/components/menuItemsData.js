2f47ebc69a6be02007971db5c7d4055eec4aadb2
elli-elka <71603746+elli-elka@users.noreply.github.com>
2024-08-21 14:54:30 +0100

    changes to menu items data

--- src/components/menuItemsData.js
+++ src/components/menuItemsData.js
@@ -22,11 +22,80 @@ export const menuItemsData = [
             title: "Fashion Editorials",
             url: "/nail-fashion-edit",
           },
+          {
+            title: "Beauty Editorials",
+            url: "/nail-beauty-edit",
+          },
+          {
+            title: "Fashion Shows",
+            url: "/nail-fashion-show",
+          },
+          {
+            title: "Commericals",
+            url: "/nail-commerical",
+          },
+          {
+            title: "Motion",
+            url: "/nail-motion",
+          },
+          {
+            title: "Hands",
+            url: "/nail-hands",
+          },
+          {
+            title: "Weddings & Parties",
+            url: "/nail-weddings-parties",
+          },
+          {
+            title: "Creative Projects",
+            url: "/nail-creative-projects",
+          },
         ],
       },
       {
         title: "Hair",
         url: "/hair",
+        submenu: [
+      {
+        title: "Nails",
+        submenu: [
+          {
+            title: "Covers",
+            url: "/haircovers",
+          },
+          {
+            title: "Fashion Editorials",
+            url: "/hair-fashion-edit",
+          },
+          {
+            title: "Beauty Editorials",
+            url: "/hair-beauty-edit",
+          },
+          {
+            title: "Fashion Shows",
+            url: "/hair-fashion-show",
+          },
+          {
+            title: "Commericals",
+            url: "/hair-commerical",
+          },
+          {
+            title: "Motion",
+            url: "/hair-motion",
+          },
+          {
+            title: "Hands",
+            url: "/hair-hands",
+          },
+          {
+            title: "Weddings & Parties",
+            url: "/hair-weddings-parties",
+          },
+          {
+            title: "Creative Projects",
+            url: "/hair-creative-projects",
+          },
+        ],
       },
     ],
   },
@@ -34,6 +103,19 @@ export const menuItemsData = [
     title: "Bookings",
     url: "/bookings",
   },
+  {
+    title: "Shop",
+    url: "/shop",
+  },
+  {
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    title: "Search",
+    url: "/search",
+  },
+},
 ];
 
 
@@ -63,4 +145,4 @@ export const menuItemsData = [
 //         ],
 //       },
 //     ],
-//   },
+//   }]
